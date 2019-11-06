deeb58e11445beeb4abf8df92000576c4845728d
richieshih <kotzen1023@gmail.com>
2019-11-06 08:25:31 +0800

    modify

--- app/src/main/java/com/seventhmoon/iottest/MainActivity.kt
+++ app/src/main/java/com/seventhmoon/iottest/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import com.google.firebase.database.*
 
 
@@ -14,6 +15,9 @@ class MainActivity : AppCompatActivity() {
     var upOnOff: Boolean = false
     var stopOnOff: Boolean = false
     var downOnOff: Boolean = false
+    var oldUp: Boolean = false
+    var oldStop: Boolean = false
+    var oldDown: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,27 @@ class MainActivity : AppCompatActivity() {
         val btnStop = findViewById<Button>(R.id.btnStop)
         val btnDown = findViewById<Button>(R.id.btnDown)
 
+        val imgUp = findViewById<ImageView>(R.id.imgUp)
+        val imgStop = findViewById<ImageView>(R.id.imgStop)
+        val imgDown = findViewById<ImageView>(R.id.imgDown)
+
+        imgUp!!.setOnClickListener {
+            if (!upOnOff) {
+                myRef.child("relay").child("ch1").child("on").setValue(true)
+                imgUp.isEnabled = false
+                imgStop.isEnabled = false
+                imgDown.isEnabled = false
+                imgUp.setImageResource(R.drawable.up_off)
+                imgStop.setImageResource(R.drawable.stop_off)
+                imgDown.setImageResource(R.drawable.down_off)
+
+
+                btnUp.isEnabled = false
+                btnStop.isEnabled = false
+                btnDown.isEnabled = false
+            }
+        }
+
         btnUp!!.setOnClickListener {
 
             /*if (upOnOff)
@@ -33,6 +58,31 @@ class MainActivity : AppCompatActivity() {
             else {*/
             if (!upOnOff) {
                 myRef.child("relay").child("ch1").child("on").setValue(true)
+                imgUp.isEnabled = false
+                imgStop.isEnabled = false
+                imgDown.isEnabled = false
+                imgUp.setImageResource(R.drawable.up_off)
+                imgStop.setImageResource(R.drawable.stop_off)
+                imgDown.setImageResource(R.drawable.down_off)
+
+                btnUp.isEnabled = false
+                btnStop.isEnabled = false
+                btnDown.isEnabled = false
+            }
+        }
+
+        imgStop!!.setOnClickListener {
+            if (!stopOnOff) {
+                myRef.child("relay").child("ch2").child("on").setValue(true)
+                imgUp.isEnabled = false
+                imgStop.isEnabled = false
+                imgDown.isEnabled = false
+                imgUp.setImageResource(R.drawable.up_off)
+                imgStop.setImageResource(R.drawable.stop_off)
+                imgDown.setImageResource(R.drawable.down_off)
+
+                btnUp.isEnabled = false
+                btnStop.isEnabled = false
                 btnDown.isEnabled = false
             }
         }
@@ -44,7 +94,32 @@ class MainActivity : AppCompatActivity() {
             else*/
             if (!stopOnOff) {
                 myRef.child("relay").child("ch2").child("on").setValue(true)
-                btnStop.isEnabled = true
+                imgUp.isEnabled = false
+                imgStop.isEnabled = false
+                imgDown.isEnabled = false
+                imgUp.setImageResource(R.drawable.up_off)
+                imgStop.setImageResource(R.drawable.stop_off)
+                imgDown.setImageResource(R.drawable.down_off)
+
+                btnUp.isEnabled = false
+                btnStop.isEnabled = false
+                btnDown.isEnabled = false
+            }
+        }
+
+        imgDown!!.setOnClickListener {
+            if (!downOnOff) {
+                myRef.child("relay").child("ch3").child("on").setValue(true)
+                imgUp.isEnabled = false
+                imgStop.isEnabled = false
+                imgDown.isEnabled = false
+                imgUp.setImageResource(R.drawable.up_off)
+                imgStop.setImageResource(R.drawable.stop_off)
+                imgDown.setImageResource(R.drawable.down_off)
+
+                btnUp.isEnabled = false
+                btnStop.isEnabled = false
+                btnDown.isEnabled = false
             }
         }
 
@@ -55,7 +130,16 @@ class MainActivity : AppCompatActivity() {
             else {*/
             if (!downOnOff) {
                 myRef.child("relay").child("ch3").child("on").setValue(true)
+                imgUp.isEnabled = false
+                imgStop.isEnabled = false
+                imgDown.isEnabled = false
+                imgUp.setImageResource(R.drawable.up_off)
+                imgStop.setImageResource(R.drawable.stop_off)
+                imgDown.setImageResource(R.drawable.down_off)
+
                 btnUp.isEnabled = false
+                btnStop.isEnabled = false
+                btnDown.isEnabled = false
             }
         }
 
@@ -69,32 +153,65 @@ class MainActivity : AppCompatActivity() {
                 Log.e(mTAG, "stopOnOff is: $stopOnOff")
                 Log.e(mTAG, "downOnOff is: $downOnOff")
 
+                if (oldUp && !upOnOff) { //true-> false
+                    imgUp.isEnabled = true
+                    imgStop.isEnabled = true
+                    imgDown.isEnabled = true
+                    imgUp.setImageResource(R.drawable.up)
+                    imgStop.setImageResource(R.drawable.stop)
+                    imgDown.setImageResource(R.drawable.down)
 
-                if (upOnOff || downOnOff ) {
-                    btnUp.isEnabled = false
-                    btnDown.isEnabled = false
-                } else {
                     btnUp.isEnabled = true
+                    btnStop.isEnabled = true
                     btnDown.isEnabled = true
+                } else if (oldStop &&  !stopOnOff) {
+                    imgUp.isEnabled = true
+                    imgStop.isEnabled = true
+                    imgDown.isEnabled = true
+                    imgUp.setImageResource(R.drawable.up)
+                    imgStop.setImageResource(R.drawable.stop)
+                    imgDown.setImageResource(R.drawable.down)
+
+                    btnUp.isEnabled = true
+                    btnStop.isEnabled = true
+                    btnDown.isEnabled = true
+                } else if (oldDown && !downOnOff) {
+                    imgUp.isEnabled = true
+                    imgStop.isEnabled = true
+                    imgDown.isEnabled = true
+                    imgUp.setImageResource(R.drawable.up)
+                    imgStop.setImageResource(R.drawable.stop)
+                    imgDown.setImageResource(R.drawable.down)
+
+                    btnUp.isEnabled = true
+                    btnStop.isEnabled = true
+                    btnDown.isEnabled = true
+                } else {
+                    Log.d(mTAG, "not true -> false")
                 }
 
-                if (upOnOff)
+
+                if (upOnOff) {
                     btnUp.text = "Up (On)"
-                else
+                } else {
                     btnUp.text = "Up (Off)"
+                }
 
-                if (stopOnOff)
+                if (stopOnOff) {
                     btnStop.text = "Stop (On)"
-                 else
+                } else {
                     btnStop.text = "Stop (Off)"
+                }
 
-                if (downOnOff)
+                if (downOnOff) {
                     btnDown.text = "Down (On)"
-                else
+                } else {
                     btnDown.text = "Down (Off)"
+                }
 
-
-
+                oldUp = upOnOff
+                oldStop = stopOnOff
+                oldDown = downOnOff
                 //btnUp.isEnabled = true
                 //btnStop.isEnabled = true
                 //btnDown.isEnabled = true
